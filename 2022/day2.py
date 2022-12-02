37e09986957ddcab43d9f823dcf583306037d4c4
ridingqwerty <george.g.koenig@gmail.com>
2022-12-02 01:44:27 -0500

    Golfed day 2 a bit

--- 2022/day2.py
+++ 2022/day2.py
@@ -5,8 +5,7 @@ for x in open("day2.input"):
  a=ord(x[0])-ord('A')
  b=ord(x[2])-ord('X')
  E+=[((a-1)%3)+1,a+4,((a+1)%3)+7][b]
- if a==b: D+=b+4;continue
- D+=[b+7,b+1][(a-1)%3==b]
+ D+=[b+4,b+1,b+7][(a-b)%3]
 
 print("Day 2, part 1:", D)
 print("Day 2, part 2:", E)
